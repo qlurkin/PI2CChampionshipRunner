430d07c591769459c5bc96559230704ec6cecdc5
Quentin Lurkin <qlurkin@gmail.com>
2021-02-11 23:46:05 +0100

    Run Championship

--- server.py
+++ server.py
@@ -3,7 +3,10 @@ from jsonNetwork import sendJSON, receiveJSON, NotAJSONObject
 from threading import Thread, Timer, Lock
 import time
 from tictactoe import Game
+import importlib
 import game
+import sys
+import json
 
 inscriptionSocket = socket.socket()
 inscriptionSocket.bind(('0.0.0.0', 3000))
@@ -44,7 +47,7 @@ def checkAllClients():
 	for address in clients:
 		checkClient(address)
 
-def subscribe(address, name):
+def subscribe(address, name, matricules):
 	status = checkClient(address)
 	if status == 'online':
 		with matchLock:
@@ -55,48 +58,53 @@ def subscribe(address, name):
 
 				clients[address] = {
 					'address': address,
-					'points': 0
+					'points': 0,
+					'badMoves': 0,
+					'matchCount': 0,
+					'matricules': matricules
 				}
 
 			clients[address]['name'] = name
 			clients[address]['status'] = status
 			
 		print('MATCH LIST:\n{}'.format('\n'.join(map(lambda address: '{}:{}'.format(address[0], address[1]), match))))
-		if len(match) > 0:
-			runMatch(match[0])
 
 
-def runMatch(players):
-	for i in range(len(players)):
-		players[i] = clients[players[i]]
+def runMatch(playersAddress):
+	players = []
+	for i in range(len(playersAddress)):
+		players.append(clients[playersAddress[i]])
 
 	state, next = Game(list(map(lambda P: P['name'], players)))
 	state['current'] = 0
 
 	print('{} VS {}'.format(players[0]['name'], players[1]['name']))
 
-	attempts = 0
+	lives = [3, 3]
+	badMoves = lambda : [3 - l for l in lives]
 	try:
-		while attempts < 3:
+		while all([l != 0 for l in lives]):
 			print('Request move from {}'.format(players[state['current']]['name']))
 			response = fetch(players[state['current']]['address'], {
 				'request': 'play',
-				'attempts': attempts,
+				'lives': lives[state['current']],
 				'state': state
 			})
 			if response['response'] == 'move':
 				print('{} play:\n{}'.format(players[state['current']]['name'], response['move']))
 				try:
 					state = next(state, response['move'])
-					attempts = 0
 				except game.BadMove:
 					print('Bad Move')
-					attempts += 1
+					lives[state['current']] -= 1
 		print(players[state['current']]['name'], 'has done too many Bad Moves')
+		return (state['current']+1)%2, badMoves()
 	except game.GameWin as e:
 		print('Winner', players[e.winner]['name'])
+		return e.winner, badMoves()
 	except game.GameDraw:
 		print('Draw')
+		return None, badMoves()
 
 def processRequest(client, address):
 	print('request from', address)
@@ -109,12 +117,15 @@ def processRequest(client, address):
 		clientAddress = (address[0], int(data['port']))
 	
 		print('Subscription received for {} with address {}'.format(data['name'], clientAddress))
+
+		if any([not isinstance(matricule, str) for matricule in data['matricules']]):
+			raise TypeError("Matricules must be strings")
 		
 		sendJSON(client, {
 			'response': 'ok'
 		})
 
-		Timer(2, subscribe, [clientAddress, data['name']]).start()
+		Timer(1, subscribe, [clientAddress, data['name'], data['matricules']]).start()
 
 	except socket.timeout:
 		sendJSON(client, {
@@ -147,16 +158,35 @@ def processClients():
 		except socket.timeout:
 			pass
 
-listenThread = Thread(target=processClients)
-listenThread.start()
 
+if __name__ == '__main__':
+	listenThread = Thread(target=processClients, daemon=True)
+	listenThread.start()
 
-try:
-	print('Ctrl-C to stop')
-	while True:
-		time.sleep(1)
-except KeyboardInterrupt:
-	print('bye')
+	Game = importlib.import_module(sys.argv[1]).Game
 
-running = False
-listenThread.join()
+	try:
+		print('Ctrl-C to stop')
+		while True:
+			players = None
+			with matchLock:
+				if len(match) > 0:
+					players = match[0]
+					match[0:1] = []
+			if players is not None:
+				winner, badMoves = runMatch(players)
+				for player, count in enumerate(badMoves):
+					clients[players[player]]['badMoves'] += count
+					clients[players[player]]['matchCount'] += 1
+					if winner is None:
+						clients[players[player]]['points'] += 1
+					elif winner == player:
+						clients[players[player]]['points'] += 3
+				with open('data.json', 'w', encoding='utf8') as file:
+					json.dump(list(clients.values()), file, indent='\t')
+			time.sleep(1)
+	except KeyboardInterrupt:
+		print('bye')
+
+	running = False
+	listenThread.join()
