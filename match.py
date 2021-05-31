caa0e4de32ffc201162e73dbaa5722c9af844211
Quentin Lurkin <qlurkin@gmail.com>
2021-05-31 14:18:23 +0200

    Better Timing

--- match.py
+++ match.py
@@ -4,6 +4,31 @@ import time
 
 getMatch, updateMatch, subscribe = Datastore(None)
 
+
+def tic(delay):
+	def decorator(fun):
+		last = None
+		def wait():
+			nonlocal last
+			if last is None:
+				elapsed = float('inf')
+			else:
+				elapsed = time.time() - last
+			if elapsed > delay:
+				last = time.time()
+			else:
+				time.sleep(delay - elapsed)
+				last = time.time()
+			
+		def wrapper(*args, **kwargs):
+			nonlocal last
+			wait()
+			return fun(*args, **kwargs)
+		return wrapper
+	return decorator
+		
+	
+
+@tic(0.5)
 def postMatchState(matchState):
 	updateMatch(lambda state: matchState)
-	#time.sleep(1)
