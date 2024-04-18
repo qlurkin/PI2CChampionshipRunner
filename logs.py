c02b766aeca61bc69f252030b025102ba546c546
Quentin Lurkin <qlurkin@gmail.com>
2024-04-18 10:52:08 +0200

    Show Debug Msg

--- logs.py
+++ logs.py
@@ -1,19 +1,20 @@
-from datetime import datetime
-import os
 import logging
+import os
+import re
 import sys
 import unicodedata
-import re
+from datetime import datetime
 
 date = datetime.now()
 
-consoleFormatter = logging.Formatter('%(levelname)s:%(name)s:%(message)s')
+consoleFormatter = logging.Formatter("%(levelname)s:%(name)s:%(message)s")
 
-fileFormatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+fileFormatter = logging.Formatter("%(asctime)s:%(levelname)s:%(name)s:%(message)s")
 
-matchFileFormatter = logging.Formatter('%(asctime)s:%(levelname)s:%(message)s')
+matchFileFormatter = logging.Formatter("%(asctime)s:%(levelname)s:%(message)s")
 
-LOGS_FOLDER = 'logs'
+LOGS_FOLDER = "logs"
+
 
 def slugify(value, allow_unicode=False):
     """
@@ -25,36 +26,45 @@ def slugify(value, allow_unicode=False):
     """
     value = str(value)
     if allow_unicode:
-        value = unicodedata.normalize('NFKC', value)
+        value = unicodedata.normalize("NFKC", value)
     else:
-        value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore').decode('ascii')
-    value = re.sub(r'[^\w\s-]', '', value.lower())
-    return re.sub(r'[-\s]+', '-', value).strip('-_')
+        value = (
+            unicodedata.normalize("NFKD", value)
+            .encode("ascii", "ignore")
+            .decode("ascii")
+        )
+    value = re.sub(r"[^\w\s-]", "", value.lower())
+    return re.sub(r"[-\s]+", "-", value).strip("-_")
+
 
 def getDateStr():
     return date.strftime("%d-%m-%Y_%Hh%M.%Ss")
 
-if not os.path.exists(LOGS_FOLDER):
-        os.mkdir(LOGS_FOLDER)
 
-mainLogFilename = os.path.join(LOGS_FOLDER, getDateStr()+'.log')
-stateFilename = os.path.join(LOGS_FOLDER, getDateStr()+'.json')
+if not os.path.exists(LOGS_FOLDER):
+    os.mkdir(LOGS_FOLDER)
+
+mainLogFilename = os.path.join(LOGS_FOLDER, getDateStr() + ".log")
+stateFilename = os.path.join(LOGS_FOLDER, getDateStr() + ".json")
+
 
 def getMainLogFilename():
     return mainLogFilename
+
 
 def getMatchFilename(match):
     folder = os.path.join(LOGS_FOLDER, getDateStr())
     if not os.path.exists(folder):
         os.mkdir(folder)
-    return os.path.join(folder, slugify('{}.log'.format(match)))
+    return os.path.join(folder, slugify("{}.log".format(match)))
+
 
 def getLogger(name):
     log = logging.getLogger(name)
     log.setLevel(logging.DEBUG)
 
     consoleHandler = logging.StreamHandler(sys.stdout)
-    consoleHandler.setLevel(logging.INFO)
+    consoleHandler.setLevel(logging.DEBUG)
     consoleHandler.setFormatter(consoleFormatter)
 
     fileHandler = logging.FileHandler(mainLogFilename)
@@ -65,6 +75,7 @@ def getLogger(name):
     log.addHandler(fileHandler)
 
     return log
+
 
 def getMatchLogger(match):
     log = logging.getLogger(str(match))
