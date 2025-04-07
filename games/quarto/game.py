5f1ee76078a48373bbc278e19e8aa7a66949b60e
Quentin Lurkin <qlurkin@gmail.com>
2025-04-07 23:12:37 +0200

    Quarto should be good

--- games/quarto/game.py
+++ games/quarto/game.py
@@ -1,106 +1,155 @@
 # NOT FINISHED !!
 
+if __name__ == "__main__":
+    import os
+    import sys
+
+    sys.path.append(os.getcwd())
+
 from games import game
 import copy
 
+
 def same(L):
-	if L[0] == None:
-		return False
-	for elem in L:
-		if elem != L[0]:
-			return False
-	return True
+    if None in L:
+        return False
+    common = L[0]
+    for elem in L[1:]:
+        common = common & elem
+    return len(common) > 0
+
 
 def getLine(board, i):
-	return board[i*4:(i+1)*4]
+    return board[i * 4 : (i + 1) * 4]
+
 
 def getColumn(board, j):
-	return [board[i] for i in range(j, 16, 4)]
+    return [board[i] for i in range(j, 16, 4)]
+
 
 # dir == 1 or -1
 def getDiagonal(board, dir):
-	start = 0 if dir == 1 else 2
-	return [board[start + i*(4+dir)] for i in range(4)]
+    start = 0 if dir == 1 else 2
+    return [board[start + i * (4 + dir)] for i in range(4)]
+
 
 def isWinning(board):
-	for i in range(4):
-		if same(getLine(board, i)):
-			return True
-		if same(getColumn(board, i)):
-			return True
-	if same(getDiagonal(board, 1)):
-		return True
-	return same(getDiagonal(board, -1))
+    for i in range(4):
+        if same(getLine(board, i)):
+            return True
+        if same(getColumn(board, i)):
+            return True
+    if same(getDiagonal(board, 1)):
+        return True
+    return same(getDiagonal(board, -1))
+
 
 def isFull(board):
-	for elem in board:
-		if elem is None:
-			return False
-	return True
+    for elem in board:
+        if elem is None:
+            return False
+    return True
+
 
 def Quarto(players):
-	if len(players) != 2:
-		raise game.BadGameInit('Tic Tac Toe must be played by 2 players')
-	
-	# Big/Small: B/S
-	# Dark/Light: D/L
-	# Empty/Full: E/F
-	# Cylinder/Prism: C/P
+    if len(players) != 2:
+        raise game.BadGameInit("Tic Tac Toe must be played by 2 players")
 
-	pieces = []
-	for size in ['B', 'S']:
-		for color in ['D', 'L']:
-			for weight in ['E', 'F']:
-				for shape in ['C', 'P']:
-					pieces.append(f'{size}{color}{weight}{shape}')
+    # Big/Small: B/S
+    # Dark/Light: D/L
+    # Empty/Full: E/F
+    # Cylinder/Prism: C/P
+    pieces = set()
+    for size in ["B", "S"]:
+        for color in ["D", "L"]:
+            for weight in ["E", "F"]:
+                for shape in ["C", "P"]:
+                    pieces.add(frozenset({size, color, weight, shape}))
 
-	state = {
-		'players': players,
-		'current': 0,
-		'board': [None] * 16
-	}
+    state = {"players": players, "current": 0, "board": [None] * 16, "piece": None}
 
-	def next(state, move):
-		newState = copy.deepcopy(state)
-		
-		try:
-			move = int(move)
-			state['board'][move]
-		except:
-			raise game.BadMove('Move must be an integer between 0 and 15 inclusive')
+    # Move
+    # {
+    #   "pos": <index>,        # between 0 and 15 or null if first move
+    #   "piece": <piece_str>,  # piece for the opponent example "BDEC"
+    # }
 
-		if state['board'][move] is not None:
-			raise game.BadMove('These place is not free')
+    def next(state, move):
+        newState = copy.deepcopy(state)
 
-		newState['board'][move] = pieces[state['current']]
+        if state["piece"] is not None:
+            if "pos" not in move:
+                raise game.BadMove("Move must contains a 'pos' key")
 
-		if isWinning(newState['board']):
-			raise game.GameWin(state['current'], newState)
+            try:
+                pos = int(move["pos"])
+                state["board"][pos]
+            except (ValueError, IndexError):
+                raise game.BadMove(
+                    "Move['pos'] must be an integer between 0 and 15 inclusive"
+                )
 
-		if isFull(newState['board']):
-			raise game.GameDraw(newState)
+            if state["board"][pos] is not None:
+                raise game.BadMove(f"These place '{pos}' is not free")
 
-		newState['current'] = (state['current'] + 1) % 2
-		return newState
+            newState["board"][pos] = frozenset(state["piece"])
 
-	return state, next
+            if isWinning(newState["board"]):
+                raise game.GameWin(state["current"], newState)
+
+            if isFull(newState["board"]):
+                raise game.GameDraw(newState)
+
+        if "piece" not in move:
+            raise game.BadMove("Move must contains a 'piece' key")
+
+        if not isinstance(move["piece"], str):
+            raise game.BadMove("Move['piece'] must be a str")
+
+        piece = frozenset(move["piece"])
+
+        if piece not in pieces:
+            raise game.BadMove(f"Piece '{move['piece']}' not available")
+
+        newState["piece"] = move["piece"]
+
+        newState["current"] = (state["current"] + 1) % 2
+        pieces.remove(piece)
+        return newState
+
+    return state, next
+
 
 Game = Quarto
 
-if __name__=='__main__':
-	def show(board):
-		for i in range(4):
-			print(getLine(board, i))
+if __name__ == "__main__":
 
-	state, next = Quarto(['LUR', 'FKY'])
-	try:
-		while True:
-			show(state['board'])
-			move = input('{} move: '.format(state['players'][state['current']]))
-			try:
-				state = next(state, move)
-			except game.BadMove as e:
-				print(e)
-	except game.GameWin as e:
-		show(e.state['board'])
-		print('{} win the game'.format(state['players'][e.winner]))
+    def show(board):
+        for i in range(4):
+            print(getLine(board, i))
+
+    def input_move(player):
+        print(f"player {player}")
+        pos = input("Pos (enter for `null`): ")
+        if len(pos) == 0:
+            pos = None
+        else:
+            pos = int(pos)
+        piece = input("Piece (enter for `null`): ")
+        if len(piece) == 0:
+            piece = None
+        return {"pos": pos, "piece": piece}
+
+    state, next = Quarto(["LUR", "FKY"])
+    try:
+        while True:
+            show(state["board"])
+            print(f"Piece: {state['piece']}")
+            move = input_move(state["players"][state["current"]])
+            try:
+                state = next(state, move)
+            except game.BadMove as e:
+                print(e)
+    except game.GameWin as e:
+        show(e.state["board"])
+        print("{} win the game".format(state["players"][e.winner]))
--- games/quarto/render.py
+++ games/quarto/render.py
@@ -3,39 +3,101 @@ import os
 
 root = os.path.dirname(__file__)
 
-cross = Image.open(os.path.join(root, 'cross.png'))
-circle = Image.open(os.path.join(root, 'circle.png'))
 
-WIDTH = 600
-HEIGHT = 600
-SIZE = (WIDTH, HEIGHT)
-LINEWIDTH = 5
+def draw_piece(draw, piece, center, side):
+    if piece is None:
+        return
+
+    size = 0.8 * side / 8  # Big
+    if "S" in piece:
+        size = 0.4 * side / 8  # Small
+    color = (0, 128, 255)  # Dark
+    if "L" in piece:
+        color = (255, 255, 0)  # Light
+    weight = color  # Full
+    if "E" in piece:
+        weight = None  # Empty
+    shape = 4
+    if "C" in piece:
+        shape = 20
+
+    draw.regular_polygon(
+        (center, size), shape, rotation=0, fill=weight, outline=color, width=10
+    )
+
 
 def render(state, side=600):
-	res = Image.new('RGBA', SIZE, (50, 50, 50))
-	draw = ImageDraw.Draw(res)
-	draw.line([(WIDTH//3, 0), (WIDTH//3, HEIGHT)], (200, 200, 200), LINEWIDTH)
-	draw.line([(2*WIDTH//3, 0), (2*WIDTH//3, HEIGHT)], (200, 200, 200), LINEWIDTH)
-	draw.line([(0, HEIGHT//3), (WIDTH, HEIGHT//3)], (200, 200, 200), LINEWIDTH)
-	draw.line([(0, 2*HEIGHT//3), (WIDTH, 2*HEIGHT//3)], (200, 200, 200), LINEWIDTH)
+    WIDTH = side
+    HEIGHT = side
+    SIZE = (WIDTH, HEIGHT)
+    LINEWIDTH = 5
 
-	if state is None:
-		return res
+    res = Image.new("RGBA", SIZE, (50, 50, 50))
+    draw = ImageDraw.Draw(res)
+    draw.line(
+        [(WIDTH // 5, HEIGHT // 5), (WIDTH // 5, HEIGHT)], (200, 200, 200), LINEWIDTH
+    )
+    draw.line(
+        [(2 * WIDTH // 5, HEIGHT // 5), (2 * WIDTH // 5, HEIGHT)],
+        (200, 200, 200),
+        LINEWIDTH,
+    )
+    draw.line(
+        [(3 * WIDTH // 5, HEIGHT // 5), (3 * WIDTH // 5, HEIGHT)],
+        (200, 200, 200),
+        LINEWIDTH,
+    )
+    draw.line(
+        [(0, 2 * HEIGHT // 5), (4 * WIDTH // 5, 2 * HEIGHT // 5)],
+        (200, 200, 200),
+        LINEWIDTH,
+    )
+    draw.line(
+        [(0, 3 * HEIGHT // 5), (4 * WIDTH // 5, 3 * HEIGHT // 5)],
+        (200, 200, 200),
+        LINEWIDTH,
+    )
+    draw.line(
+        [(0, 4 * HEIGHT // 5), (4 * WIDTH // 5, 4 * HEIGHT // 5)],
+        (200, 200, 200),
+        LINEWIDTH,
+    )
 
-	startx = WIDTH//6 - cross.size[0]//2
-	starty = HEIGHT//6 - cross.size[1]//2
-	stepx = WIDTH//3
-	stepy = HEIGHT//3
-	for i, elem in enumerate(state['board']):
-		x = i%3
-		y = i//3
-		if elem == 'X':
-			res.paste(cross, (startx+x*stepx, starty+y*stepy), cross)
-		elif elem == 'O':
-			res.paste(circle, (startx+x*stepx, starty+y*stepy), circle)
+    if state is None:
+        return res
 
-	return res
+    startx = WIDTH // 10
+    starty = HEIGHT // 10
+    stepx = WIDTH // 5
+    stepy = HEIGHT // 5
 
-if __name__=='__main__':
-	image = render(None)
-	image.save('image.png')
+    draw_piece(draw, state["piece"], (startx + 4 * stepx, starty), 4 * side // 5)
+
+    starty += stepy
+
+    for i, elem in enumerate(state["board"]):
+        x = i % 4
+        y = i // 4
+
+        draw_piece(draw, elem, (startx + x * stepx, starty + y * stepy), 4 * side // 5)
+
+    return res
+
+
+if __name__ == "__main__":
+    pieces = set()
+    for size in ["B", "S"]:
+        for color in ["D", "L"]:
+            for weight in ["E", "F"]:
+                for shape in ["C", "P"]:
+                    pieces.add(frozenset({size, color, weight, shape}))
+
+    state = {
+        "players": ["LUR", "FKY"],
+        "current": 0,
+        "board": list(pieces),
+        "piece": "BDEC",
+    }
+
+    image = render(state)
+    image.save("image.png")
