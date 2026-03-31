a90123463a09e796e4b824bbcdec0c41b5b2a594
Quentin Lurkin <qlurkin@gmail.com>
2026-03-31 14:07:05 +0200

    Custom win message

--- games/game.py
+++ games/game.py
@@ -1,42 +1,50 @@
 class GameEnd(Exception):
-	def __init__(self, lastState):
-		self.__state = lastState
+    def __init__(self, lastState):
+        self.__state = lastState
 
-	@property
-	def state(self):
-		return self.__state
+    @property
+    def state(self):
+        return self.__state
 
-	def __str__(self):
-		return 'Game Over'
+    def __str__(self) -> str:
+        return "Game Over"
+
 
 class GameWin(GameEnd):
-	def __init__(self, winner, lastState):
-		super().__init__(lastState)
-		self.__winner = winner
+    def __init__(self, winner, lastState, msg=None):
+        super().__init__(lastState)
+        self.__winner = winner
+        if msg is None:
+            msg = "{} win the game".format(self.winner)
+        self.__msg = msg
 
-	@property
-	def winner(self):
-		return self.__winner
+    @property
+    def winner(self):
+        return self.__winner
 
-	def __str__(self):
-		return super().__str__() + ': {} win the game'.format(self.winner)
+    def __str__(self):
+        return super().__str__() + ": " + self.__msg
+
 
 class BadMove(Exception):
-	pass
+    pass
+
 
 class GameDraw(GameEnd):
-	def __init__(self, lastState):
-		super().__init__(lastState)
+    def __init__(self, lastState):
+        super().__init__(lastState)
 
-	def __str__(self):
-		return super().__str__() + ': Draw'
+    def __str__(self):
+        return super().__str__() + ": Draw"
+
 
 class GameLoop(GameDraw):
-	def __init__(self, lastState):
-		super().__init__(lastState)
+    def __init__(self, lastState):
+        super().__init__(lastState)
 
-	def __str__(self):
-		return super().__str__() + ': Stopped because of lopping behavior'
+    def __str__(self):
+        return super().__str__() + ": Stopped because of lopping behavior"
+
 
 class BadGameInit(Exception):
-	pass
+    pass
